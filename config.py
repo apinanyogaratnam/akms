2ee7d4b6b75907799b776214ae2229b73b50a5b3
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-11 18:01:13 -0500

    feat: created config module for env vars

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+host = os.environ.get("DATABASE_HOST")
+db = os.environ.get("DATABASE_NAME")
+user = os.environ.get("DATABASE_USER")
+password = os.environ.get("DATABASE_PASSWORD")
