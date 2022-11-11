6a2be739f983e38bda71e5b5fddfe5baa269929c
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-11 18:22:01 -0500

    refactor: create api key endpoint

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 from akms_hash import hash_api_key
 from uuid import uuid4
 from api.utility import save_api_key_to_db, InsertFailedError
@@ -17,13 +18,20 @@ def read_item(item_id: int, q: str | None = None):
     return {"item_id": item_id, "q": q}
 
 
+class Item(BaseModel):
+    user_id: str
+    name: str
+    description: str | None = Field(default=None, title="The description of the item")
+    plan: str
+
+
 @app.post("/create_api_key")
-def create_api_key(key: str, name: str, description: str, plan: str):
+def create_api_key(item: Item = Body(...)):
     api_key = str(uuid4())
     hashed_api_key = hash_api_key(api_key)
     # save hashed_api_key to db
     try:
-        save_api_key_to_db(hashed_api_key, name, description, plan)
+        save_api_key_to_db(item.user_id, hashed_api_key, item.name, item.description, item.plan)
     except InsertFailedError as error:
         return {"error": str(error)}, HTTPStatus.INTERNAL_SERVER_ERROR.value
     return {"api_key": api_key, "hashed_api_key": hashed_api_key}
