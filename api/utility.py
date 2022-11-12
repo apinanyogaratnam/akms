797e64d78a1681ace05b403ff801d4dfb69edaf1
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-12 00:36:59 -0500

    feat: created is_valid_api_key function

--- api/utility.py
+++ api/utility.py
@@ -1,5 +1,7 @@
 import psycopg2
 from config import host, db, user, password
+from akms_hash import hash_api_key
+import pandas as pd
 
 
 class InsertFailedError(Exception):
@@ -39,3 +41,21 @@ def save_api_key_to_db(user_id: str, hashed_api_key: str, name: str, description
     finally:
         cursor.close()
         connection.close()
+
+
+def is_valid_api_key(api_key: str) -> bool:
+    hashed_api_key = hash_api_key(api_key)
+
+    connection = psycopg2.connect(
+        host=host,
+        database=db,
+        user=user,
+        password=password
+    )
+    query_api_key = """
+        SELECT key FROM api_keys WHERE key = %s;
+    """
+    df = pd.read_sql(query_api_key, connection, params=(hashed_api_key,))
+    connection.close()
+
+    return not df.empty
