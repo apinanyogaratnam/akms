4d43971c09a6ce74257d045ca5acc31dafb722fc
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-12 00:49:42 -0500

    fix: decoding instead of casting bytes to str

--- api/utility.py
+++ api/utility.py
@@ -44,8 +44,8 @@ def save_api_key_to_db(user_id: str, hashed_api_key: str, name: str, description
 
 
 def is_valid_api_key(api_key: str) -> bool:
-    hashed_api_key = str(hash_api_key(api_key))
-
+    hashed_api_key = hash_api_key(api_key).decode("utf-8")
+    print('hashed_api_key', hashed_api_key)
     connection = psycopg2.connect(
         host=host,
         database=db,
