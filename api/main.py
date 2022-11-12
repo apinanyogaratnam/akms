13a7ece8cd52268166dc6a77bc62e998f3b9efb2
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-12 16:03:32 -0500

    refactor: removed old endpoints

--- api/main.py
+++ api/main.py
@@ -8,16 +8,6 @@ from http import HTTPStatus
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"data": "api is running"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-
 class Item(BaseModel):
     user_id: str
     name: str
