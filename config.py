da3029f1d85fdbe81caed9faa81acdc3dd069d16
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-11 23:23:03 -0500

    fix: using or syntax instead of default values

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-host = os.environ.get("DATABASE_HOST", "localhost")
-db = os.environ.get("DATABASE_NAME", "postgres")
-user = os.environ.get("DATABASE_USER", "postgres")
-password = os.environ.get("DATABASE_PASSWORD", "postgres")
+host = os.environ.get("DATABASE_HOST") or "localhost"
+db = os.environ.get("DATABASE_NAME") or "postgres"
+user = os.environ.get("DATABASE_USER") or "postgres"
+password = os.environ.get("DATABASE_PASSWORD") or "postgres"
