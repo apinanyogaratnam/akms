0e12bde7333bb7c16cac92ef691f760091741c71
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-18 11:00:43 -0500

    refactor: removed default values

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-host = os.environ.get("DATABASE_HOST") or "localhost"
-db = os.environ.get("DATABASE_NAME") or "postgres"
-user = os.environ.get("DATABASE_USER") or "postgres"
-password = os.environ.get("DATABASE_PASSWORD") or "postgres"
+host = os.environ.get("DATABASE_HOST")
+db = os.environ.get("DATABASE_NAME")
+user = os.environ.get("DATABASE_USER")
+password = os.environ.get("DATABASE_PASSWORD")
