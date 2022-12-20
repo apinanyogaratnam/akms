eaffa35ca6629edb76579c8c9afbb8ca56b1cb12
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-20 07:57:05 -0500

    test: testing create api key endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,26 @@
 from fastapi.testclient import TestClient
 from api.main import app
+from http import HTTPStatus
 
 client = TestClient(app)
 
 
-def test_read_main():
+def test_read_home():
     response = client.get("/")
-    assert response.status_code == 200
+    assert response.status_code == HTTPStatus.OK.value
     assert response.json() == {'status': 'running'}
+
+
+def test_create_api_key():
+    user_id = 'test@test.com'
+    name = 'main api key'
+    description = 'production api key'
+    role = 'admin'
+
+    response = client.post("/create_api_key", json={'user_id': user_id, 'name': name, 'description': description, 'role': role})
+
+    actual_api_key = response.json()['api_key']
+
+    assert response.status_code == HTTPStatus.CREATED.value
+    assert isinstance(actual_api_key, str)
+    assert len(response.json()['api_key']) == 32
