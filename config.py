c3c83ce7dc0dbd2d08a034124f4099f6cbae22d7
apinanyogaratnam <apinanapinan@icloud.com>
2022-12-20 22:35:23 -0500

    refactor: getting port from config file

--- config.py
+++ config.py
@@ -8,3 +8,9 @@ host = os.environ.get("DATABASE_HOST")
 db = os.environ.get("DATABASE_NAME")
 user = os.environ.get("DATABASE_USER")
 password = os.environ.get("DATABASE_PASSWORD")
+port = os.environ.get("DATABASE_PORT")
+
+try:
+    port = int(port)
+except ValueError as error:
+    raise EnvironmentError(f"Invalid port {port} from environment") from error
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
 import os
 
 import uvicorn
-from dotenv import load_dotenv
 
-load_dotenv()
+from config import port
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT")
-
-    try:
-        port = int(port)
-    except ValueError as e:
-        raise EnvironmentError(f"Invalid port {port} from environment") from e
-
     uvicorn.run("api.main:app", host="0.0.0.0", port=port, reload=os.environ.get("ENVIRONMENT") == "development")
